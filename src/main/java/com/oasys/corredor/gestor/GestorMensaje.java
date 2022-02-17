027667422f7144466985e82d37f70056a026ed2a
User <User@User-PC>
2022-02-17 12:16:31 -0500

    udpate 2022

--- src/main/java/com/oasys/corredor/gestor/GestorMensaje.java
+++ src/main/java/com/oasys/corredor/gestor/GestorMensaje.java
@@ -33,12 +33,15 @@ public class GestorMensaje {
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
+//        props.put("mail.smtp.starttls.required", "true");
+//        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
 //        props.put("mail.smtp.host", "smtp.gmail.com");
 //        props.put("mail.smtp.host", "smtp-mail.outlook.com");
         props.put("mail.smtp.host", host);
 //        props.put("mail.smtp.host", "smtp.office365.com");
 
         props.put("mail.smtp.port", "587");
+//        props.put("mail.smtp.port", "465");
 
         Session session = Session.getInstance(props,
                 new javax.mail.Authenticator() {
